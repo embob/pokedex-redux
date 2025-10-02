897f82ba614ae0a4a03989715735730596449870
Emma Karayiannis <emma.karayiannis@gmail.com>
2025-10-02 19:58:38 +0100

    Add imgs

---
+++ src/assets/img/bg/index.js
@@ -0,0 +1,8 @@
+import fire from "./fire.jpg";
+import ghost from "./ghost.jpg";
+import grass from "./grass.jpg";
+import poison from "./poison.jpg";
+import water from "./water.jpg";
+
+const backgroundImages = [ fire, ghost, grass, poison, water ];
+export default backgroundImages;
